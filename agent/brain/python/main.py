56e12b8592d8dbe05e1b5bf4f3139424a08f48fd
Ludovic Jean-Baptiste <ludovjb@gmail.com>
2025-08-03 14:34:54 +0200

    fix lint python

--- agent/brain/python/main.py
+++ agent/brain/python/main.py
@@ -1,10 +1,12 @@
 import asyncio
 from nats.aio.client import Client as NATS
 
+
 async def main():
     nc = NATS()
     await nc.connect("nats://nats:4222")
     print("nats connected!!")
+
     async def message_handler(msg):
         print(f"Received signal: {msg.data.decode()}")
         print("Starting frontend app...")
@@ -15,6 +17,7 @@ async def main():
     print("Frontend waiting for backend...")
     while nc.is_connected:
         await asyncio.sleep(1)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
